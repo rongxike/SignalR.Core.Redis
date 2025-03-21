11b9ef4e4a84dc59d5a2a645ac9cdd86760836bc
miller <millernguyen.mbs@gmail.com>
2025-03-21 19:24:07 +0800

    updated projects, tested with 2 projects pararell

---
+++ Microsoft.AspNet.SignalR.Samples2/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+using System.Security.Principal;
+using System.Web.Routing;
+using System.Web.Security;
+using Microsoft.AspNet.SignalR.Samples;
+
+namespace Microsoft.AspNet.SignalR.Samples
+{
+    public class Global : System.Web.HttpApplication
+    {
+      
+    }
+}
