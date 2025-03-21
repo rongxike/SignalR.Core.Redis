e3cb3f65d1af204250f6fda4874e98fe6a5397c3
miller <millernguyen.mbs@gmail.com>
2025-03-21 18:38:14 +0800

    ok, all good, tested with memurai

---
+++ SignalR.Core.Server/Program.cs
@@ -0,0 +1,30 @@
+using SignalR.Core.Server.hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddStackExchangeRedis("localhost:6379");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
---
+++ SignalR.Core.Server/hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Core.Server.hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
